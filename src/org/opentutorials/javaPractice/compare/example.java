471f77958973e98daae3ea72974627720068b6ff
seonwoong <lso5936@gmail.com>
2022-03-04 17:02:13 +0900

    day5

---
+++ src/org/opentutorials/javaPractice/compare/example.java
@@ -0,0 +1,17 @@
+package org.opentutorials.javaPractice.compare;
+
+public class example {
+	static double num = 3.14;
+
+	public static void main(String[] args) {
+		System.out.println("example");
+
+	}
+
+}
+class ExampleDemo1{
+	public static void main(String[] args) {
+		example number = new example();
+		System.out.println(number.num);
+	}
+} 
--- src/org/opentutorials/javaPractice/compare/scanner.java
+++ src/org/opentutorials/javaPractice/compare/scanner.java
@@ -1,12 +1,14 @@
 package org.opentutorials.javaPractice.compare;
 
 import java.util.Scanner;
-public class arr {
+public class scanner{
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int i = sc.nextInt();
+		int i = sc.nextInt();//숫자
+		String str = sc.next();//문자
 		System.out.println(i * 1000);
+		System.out.println(str);
 		sc.close();
 	}
 
