14770f545c33c81c1a461aea2c5caaa95241088c
seonwoong <lso5936@gmail.com>
2022-03-08 19:15:15 +0900

    day5

--- src/org/opentutorials/javaPractice/compare/example.java
+++ src/org/opentutorials/javaPractice/compare/example.java
@@ -2,6 +2,7 @@ package org.opentutorials.javaPractice.compare;
 
 public class example {
 	static double num = 3.14;
+	static int number = 10;
 
 	public static void main(String[] args) {
 		System.out.println("example");
@@ -13,5 +14,6 @@ class ExampleDemo1{
 	public static void main(String[] args) {
 		example number = new example();
 		System.out.println(number.num);
+		System.out.println(number.number);
 	}
 } 
